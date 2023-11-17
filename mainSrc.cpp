ba706a099e10f0ce6c86344967e84dedfa0fa82b
Adam Watlers <adamwalters2006@gmail.com>
2023-11-17 11:56:24 -0500

    added game into main function

--- mainSrc.cpp
+++ mainSrc.cpp
@@ -1,1 +1,13 @@
 // main code program to run
+#include "rps_game.h"
+using namespace std;
+
+int main() {
+    RockPaperScissors game("Computer");
+
+    for (int i = 0; i < 3; ++i) {
+        game.playRound();
+    }
+
+    return 0;
+}
