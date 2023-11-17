2b1ac1043160a181b000c8c32840b997367a25e0
Adam Watlers <adamwalters2006@gmail.com>
2023-11-17 11:52:02 -0500

    Created header file to define RPS game functions

--- rps_game.h
+++ rps_game.h
@@ -1,1 +1,25 @@
 // header file for game
+#ifndef RPS_GAME_H
+#define RPS_GAME_H
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+class RockPaperScissors {
+public:
+    RockPaperScissors(const std::string& opponentName);
+    void playRound();
+
+private:
+    std::string opponent;
+    int userChoice;
+    int opponentChoice;
+
+    int getUserChoice();
+    void printChoices();
+    void determineWinner();
+};
+
+#endif // RPS_GAME_H
